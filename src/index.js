da45f9153fc2c1dcc391255250b31193ea06117b
Bukacz <mbukacz@gmail.com>
2019-12-19 21:51:06 +0100

    hero fade out animation on button click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './scss/style.scss';
+
+window.addEventListener('DOMContentLoaded', function() {
+  const fetchBtn = document.getElementById('fetchBtn');
+  fetchBtn.addEventListener('click', () => {
+    document.querySelectorAll('.hero__image').forEach(image => {
+      image.classList.add('faded--monsters');
+    });
+    document.querySelector('.hero__header').classList.add('faded--hero');
+  });
+});
