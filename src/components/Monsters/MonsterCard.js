4d84bb7b02263c32953e7b5ca981a54d3d7561eb
marlucz <marcin_luczak@hotmail.com>
2020-05-01 00:19:24 +0200

    MonsterCard - change monster stats to be rounded on assignment rather than inside rendered elements

--- src/components/Monsters/MonsterCard.js
+++ src/components/Monsters/MonsterCard.js
@@ -11,9 +11,6 @@ const MonsterCard = () => {
     selectedMonster: { name, description, statistics, images }
   } = useContext(MonstersContext);
 
-  const powerBar = statistics ? statistics.power : '';
-  const frequencyBar = statistics ? statistics.frequency : '';
-  const dangerBar = statistics ? statistics.danger : '';
   // prevents stats to be displayed with decimal fractions
   const roundStat = stat => {
     return Math.floor(stat * 100);
@@ -21,6 +18,13 @@ const MonsterCard = () => {
   const getRandomBgcUrl = array => {
     return array[Math.floor(Math.random() * array.length)];
   };
+
+  const powerBar = statistics ? roundStat(statistics.power) : '';
+  const frequencyBar = statistics ? roundStat(statistics.frequency) : '';
+  const dangerBar = statistics ? roundStat(statistics.danger) : '';
+
+  const loadingMonsterUrl =
+    'https://miro.medium.com/max/796/1*Jj7zXRWMuSW7P0ttFHJtZQ.gif';
 
   return (
     <>
@@ -38,30 +42,20 @@ const MonsterCard = () => {
           </div>
           <ul className={styles.stats}>
             <li className={styles.statsItem}>
-              <label htmlFor="powerBar">Power: {roundStat(powerBar)}%</label>
-              <progress
-                id="powerBar"
-                max={100}
-                value={roundStat(powerBar)}
-              ></progress>
+              <label htmlFor="powerBar">Power: {powerBar}%</label>
+              <progress id="powerBar" max={100} value={powerBar}></progress>
             </li>
             <li className={styles.statsItem}>
-              <label htmlFor="frequencyBar">
-                Frequency: {roundStat(frequencyBar)}%
-              </label>
+              <label htmlFor="frequencyBar">Frequency: {frequencyBar}%</label>
               <progress
                 id="frequencyBar"
                 max={100}
-                value={roundStat(frequencyBar)}
+                value={frequencyBar}
               ></progress>
             </li>
             <li className={styles.statsItem}>
-              <label htmlFor="dangerBar">Danger: {roundStat(dangerBar)}%</label>
-              <progress
-                id="dangerBar"
-                max={100}
-                value={roundStat(dangerBar)}
-              ></progress>
+              <label htmlFor="dangerBar">Danger: {dangerBar}%</label>
+              <progress id="dangerBar" max={100} value={dangerBar}></progress>
             </li>
           </ul>
           <p className={styles.description}>{description}</p>
@@ -70,7 +64,7 @@ const MonsterCard = () => {
         <>
           <MessageHandler
             image={true}
-            src={'https://miro.medium.com/max/796/1*Jj7zXRWMuSW7P0ttFHJtZQ.gif'}
+            src={loadingMonsterUrl}
             alt={'monster gif'}
           >
             No Monster Selected
